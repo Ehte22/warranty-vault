ab4b3103ac0d0be4a9214f21e7174798ef732c84
Ehte22 <syedehteshamali22@gmail.com>
2025-04-02 15:44:48 +0530

    google login url update

--- client/src/components/GoogleLoginButton.tsx
+++ client/src/components/GoogleLoginButton.tsx
@@ -1,10 +1,10 @@
 import React from "react";
 import { Button } from "@mui/material";
-import { FcGoogle } from "react-icons/fc"; // Google icon
+import { FcGoogle } from "react-icons/fc";
 
 const GoogleLoginButton: React.FC = () => {
     const handleGoogleLogin = () => {
-        window.location.href = "http://localhost:5000/api/v1/auth/google"
+        window.location.href = `${import.meta.env.VITE_BACKEND_URL}/api/v1/auth/google`
     };
 
     return (
